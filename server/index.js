c0e2bcd256c649afe2cfefbc433e0a68462a07dc
Yuvadi29 <adityatrivedi612@gmail.com>
2022-11-27 00:18:07 +0530

    Connection to Database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const port = 5000;
+const dotenv = require('dotenv');
 
-app.use('/', (req,res) => {
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}).then(console.log('Connected to Database Successfully'))
+    .catch((err) => console.log(err));
+
+app.use('/', (req, res) => {
     console.log("Main URL")
 });
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Server is Running at port https://localhost:${port}`);
 })
