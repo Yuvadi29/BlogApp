012983a2ca6361be8eaeb31b6aa20eb51c7fad31
Yuvadi29 <adityatrivedi612@gmail.com>
2022-11-27 17:54:23 +0530

    Login and Register Routes

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const dotenv = require('dotenv');
+const authRoute = require('./routes/Auth');
 //DotENV path
 
 dotenv.config();
+app.use(express.json()); //Imp for testing of API
 
 mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
@@ -13,9 +15,7 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(console.log('Connected to Database Successfully'))
     .catch((err) => console.log(err));
 
-app.use('/', (req, res) => {
-    console.log("Main URL")
-});
+app.use('/api/auth', authRoute);
 
 app.listen(port, () => {
     console.log(`Server is Running at port https://localhost:${port}`);
--- server/models/User.js
+++ server/models/User.js
@@ -16,6 +16,10 @@ const UserSchema = new mongoose.Schema(
             type: String,
             required: true
         },
+        mobileNo: {
+            type: Number,
+            required: true,
+        },
         ProfileImage: {
             type: String,
             default: "",
